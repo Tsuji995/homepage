f90291fe29fc337874824507684ae46dd014a4d8
Tomonari <shizhicheng651@gmail.com>
2024-01-23 15:12:26 +0900

    body menu add

--- src/pages/Course.js
+++ src/pages/Course.js
@@ -1,11 +1,47 @@
 import React from 'react'
 
+import styled from 'styled-components'
+
+const MenuList = styled.div`
+ 
+`
+
+const PriceList = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  font-size: 34px;
+  line-height: 0;
+  font-weight: bold;
+`
+
+const Span = styled.span`
+  font-size: 14px
+`
+
+
 export const Course = () => {
   return (
-    <div>
+    <MenuList>
       <div>
         <h1>BODY MASSAGE</h1>
       </div>
-    </div>
+      <PriceList>
+        <p>30min</p>
+        <p>1,980yen<Span>(with tax)</Span></p>
+      </PriceList>
+      <PriceList>
+        <p>60min</p>
+        <p>3,278yen<Span>(with tax)</Span></p>
+      </PriceList>
+      <PriceList>
+        <p>90min</p>
+        <p>4,950yen<Span>(with tax)</Span></p>
+      </PriceList>
+      <PriceList>
+        <p>120min</p>
+        <p>6,490yen<Span>(with tax)</Span></p>
+      </PriceList>
+    </MenuList>
   )
 }
